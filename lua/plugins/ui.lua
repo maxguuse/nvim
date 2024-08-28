013bcc64ffdbfee3b41e710935c6d63cbf4f0fab
maxguuse <max.pink.goose@gmail.com>
2024-08-29 02:21:58 +0300

    Added scrollbar and gitsigns

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -61,4 +61,16 @@ return {
 			})
 		end,
 	},
+	{
+		"petertriho/nvim-scrollbar",
+		dependencies = {
+			"kevinhwang91/nvim-hlslens",
+			"lewis6991/gitsigns.nvim",
+		},
+		config = function()
+			require("scrollbar").setup({})
+			require("scrollbar.handlers.search").setup({})
+			require("scrollbar.handlers.gitsigns").setup()
+		end,
+	},
 }
