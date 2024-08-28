d9b0b2b26274e07b2c69442a878e5b97cc859025
maxguuse <max.pink.goose@gmail.com>
2024-08-29 01:43:06 +0300

    Added statuscol.nvim for ui

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -43,4 +43,22 @@ return {
 			render = "compact",
 		},
 	},
+	{
+		"luukvbaal/statuscol.nvim",
+		config = function()
+			local builtin = require("statuscol.builtin")
+			require("statuscol").setup({
+				setopt = true,
+				segments = {
+					{ text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+					{ text = { "%s" }, click = "v:lua.ScSa" },
+					{
+						text = { builtin.lnumfunc, " " },
+						condition = { true, builtin.not_empty },
+						click = "v:lua.ScLa",
+					},
+				},
+			})
+		end,
+	},
 }
