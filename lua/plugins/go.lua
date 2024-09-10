90486fdd510ec20c94278ecc7882b0efd9ee17b3
maxguuse <max.pink.goose@gmail.com>
2024-09-10 20:48:32 +0300

    feat: refactoring.nvim

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -1,14 +1,56 @@
 return {
-	"ray-x/go.nvim",
-	dependencies = { -- optional packages
-		"ray-x/guihua.lua",
-		"neovim/nvim-lspconfig",
-		"nvim-treesitter/nvim-treesitter",
+	{
+		"ray-x/go.nvim",
+		dependencies = { -- optional packages
+			"ray-x/guihua.lua",
+			"neovim/nvim-lspconfig",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		config = function()
+			require("go").setup()
+		end,
+		event = { "CmdlineEnter" },
+		ft = { "go", "gomod" },
+		build = ':lua require("go.install").update_all_sync()',
 	},
-	config = function()
-		require("go").setup()
-	end,
-	event = { "CmdlineEnter" },
-	ft = { "go", "gomod" },
-	build = ':lua require("go.install").update_all_sync()',
+	{
+		"ThePrimeagen/refactoring.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		config = function()
+			require("refactoring").setup({
+				prompt_func_return_type = {
+					go = true,
+					java = false,
+
+					cpp = false,
+					c = false,
+					h = false,
+					hpp = false,
+					cxx = false,
+				},
+				prompt_func_param_type = {
+					go = true,
+					java = false,
+
+					cpp = false,
+					c = false,
+					h = false,
+					hpp = false,
+					cxx = false,
+				},
+				printf_statements = {},
+				print_var_statements = {},
+				show_success_message = false,
+			})
+
+			require("telescope").load_extension("refactoring")
+
+			vim.keymap.set({ "n", "x" }, "<leader>rr", function()
+				require("telescope").extensions.refactoring.refactors()
+			end)
+		end,
+	},
 }
