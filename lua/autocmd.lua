e0d53777b4d5f9744461469d4a65ea20042b1574
maxguuse <max.pink.goose@gmail.com>
2024-09-11 15:23:50 +0300

    fix: changed directory for autocmd for exiting to cwd

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -1,6 +1,6 @@
 vim.api.nvim_create_autocmd("BufEnter", {
 	callback = function()
-		local last_dir_file = os.getenv("HOME") .. "/.last_nvim_dir"
+		local last_dir_file = os.getenv("HOME") .. "/.config/nvim/.last_nvim_dir"
 		local current_dir = vim.fn.getcwd()
 		vim.fn.writefile({ current_dir }, last_dir_file)
 	end,
