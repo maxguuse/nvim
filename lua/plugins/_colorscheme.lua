ef7043914fb38604a80a54294e4c3e8598fafb15
maxguuse <max.pink.goose@gmail.com>
2024-10-26 17:29:35 +0300

    feat: theme switching

--- lua/plugins/_colorscheme.lua
+++ lua/plugins/_colorscheme.lua
@@ -1,12 +1,45 @@
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		require("catppuccin").setup({
-			flavour = "mocha",
-		})
-		vim.cmd("colorscheme catppuccin")
-	end,
+	{
+		"zaldih/themery.nvim",
+		lazy = false,
+		config = function()
+			require("themery").setup({
+				themes = { "catppuccin", "poimandres", "lackluster" },
+				livePreview = true,
+			})
+		end,
+	},
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				flavour = "mocha",
+			})
+			vim.cmd("colorscheme catppuccin")
+		end,
+	},
+	{
+		"olivercederborg/poimandres.nvim",
+		name = "poimandres",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("poimandres").setup({})
+			vim.cmd("colorscheme poimandres")
+		end,
+	},
+	{
+		"slugbyte/lackluster.nvim",
+		name = "lackluster",
+		lazy = false,
+		priority = 1000,
+		init = function()
+			-- vim.cmd.colorscheme("lackluster")
+			-- vim.cmd.colorscheme("lackluster-hack") -- my favorite
+			vim.cmd.colorscheme("lackluster-mint")
+		end,
+	},
 }
