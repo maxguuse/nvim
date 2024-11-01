3bd68358cdc5bbea9f419446b891c84e9f34b0f0
maxguuse <max.pink.goose@gmail.com>
2024-11-01 18:16:20 +0300

    fix: inactive lualine style

--- lua/plugins/bars.lua
+++ lua/plugins/bars.lua
@@ -33,7 +33,7 @@ return {
 			sections = {
 				lualine_a = { "mode" },
 				lualine_b = { "branch", "diff", "diagnostics" },
-				lualine_c = {},
+				lualine_c = { "filename" },
 				lualine_x = { "encoding", "fileformat", "filetype" },
 				lualine_y = { "progress" },
 				lualine_z = { "location" },
