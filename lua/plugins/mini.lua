9deea2e20257f8ad0c5235ad07a816263fd4f4de
maxguuse <max.pink.goose@gmail.com>
2024-12-13 12:28:36 +0300

    refactor: replaced colorizer and todo-comments with mini.hipatterns

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -49,6 +49,23 @@ return {
 		end,
 	},
 	{
+		"echasnovski/mini.hipatterns",
+		version = false,
+		config = function()
+			local hipatterns = require("mini.hipatterns")
+			hipatterns.setup({
+				highlighters = {
+					fixme = { pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme" },
+					hack = { pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack" },
+					todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
+					note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
+
+					hex_color = hipatterns.gen_highlighter.hex_color(),
+				},
+			})
+		end,
+	},
+	{
 		"echasnovski/mini.statusline",
 		version = false,
 		dependencies = {
