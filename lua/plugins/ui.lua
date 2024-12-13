54002cb15a8389b6b3c5cc9168d2994073664ef5
maxguuse <max.pink.goose@gmail.com>
2024-12-13 08:47:41 +0300

    refactor: moved dropbar.nvim in ui.lua

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,3 +1,13 @@
+local function get_relative_path()
+	local workspace = vim.lsp.buf.list_workspace_folders()[1]
+	if workspace and workspace ~= "" then
+		return workspace
+	end
+
+	local buffer_path = vim.fn.getcwd()
+	return buffer_path
+end
+
 return {
 	{
 		"stevearc/dressing.nvim",
@@ -8,5 +18,19 @@ return {
 		config = function()
 			require("scrollbar").setup({})
 		end,
+	},
+	{
+		"Bekaboo/dropbar.nvim",
+		lazy = true,
+		event = {
+			"BufEnter",
+		},
+		opts = {
+			sources = {
+				path = {
+					relative_to = get_relative_path,
+				},
+			},
+		},
 	},
 }
