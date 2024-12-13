f484518913aa07364aaff6158c1e5e230cc23a7b
maxguuse <max.pink.goose@gmail.com>
2024-12-13 09:01:04 +0300

    refactor: removed dropbar

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,13 +1,3 @@
-local function get_relative_path()
-	local workspace = vim.lsp.buf.list_workspace_folders()[1]
-	if workspace and workspace ~= "" then
-		return workspace
-	end
-
-	local buffer_path = vim.fn.getcwd()
-	return buffer_path
-end
-
 return {
 	{
 		"stevearc/dressing.nvim",
@@ -18,19 +8,5 @@ return {
 		config = function()
 			require("scrollbar").setup({})
 		end,
-	},
-	{
-		"Bekaboo/dropbar.nvim",
-		lazy = true,
-		event = {
-			"BufEnter",
-		},
-		opts = {
-			sources = {
-				path = {
-					relative_to = get_relative_path,
-				},
-			},
-		},
 	},
 }
