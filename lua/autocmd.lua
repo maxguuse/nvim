615d047f84f0575ade13188c399702e1852af07e
maxguuse <max.pink.goose@gmail.com>
2024-12-24 13:58:08 +0300

    fix: added error message for situation when KITTY_LISTEN_ON env not specified

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -15,6 +15,10 @@ vim.api.nvim_create_autocmd("UIEnter", {
 	group = "KittyStyles",
 	callback = function()
 		local sock = os.getenv("KITTY_LISTEN_ON")
+		if sock == nil then
+			error("KITTY_LISTEN_ON not specified")
+			return
+		end
 
 		vim.system({ "kitty", "@", "--to", sock, "set-spacing", "padding=0" })
 	end,
@@ -23,6 +27,11 @@ vim.api.nvim_create_autocmd("UILeave", {
 	group = "KittyStyles",
 	callback = function()
 		local sock = os.getenv("KITTY_LISTEN_ON")
+		if sock == nil then
+			error("KITTY_LISTEN_ON not specified")
+			return
+		end
+
 		local obj = vim.system({ "awk", "/^background/ {print $2}", "/home/ggoose/.config/kitty/theme.conf" }):wait()
 		local color = string.format("background=%s", obj.stdout)
 
@@ -33,9 +42,14 @@ vim.api.nvim_create_autocmd("UILeave", {
 vim.api.nvim_create_autocmd("ColorScheme", {
 	group = "KittyStyles",
 	callback = function()
+		local sock = os.getenv("KITTY_LISTEN_ON")
+		if sock == nil then
+			error("KITTY_LISTEN_ON not specified")
+			return
+		end
+
 		local hl_id = vim.api.nvim_get_hl_id_by_name("Normal")
 		local rgb = vim.api.nvim_get_hl(0, { id = hl_id })
-		local sock = os.getenv("KITTY_LISTEN_ON")
 		local color = string.format("background=#%06x", rgb.bg)
 
 		vim.system({ "kitty", "@", "--to", sock, "set-colors", color })
