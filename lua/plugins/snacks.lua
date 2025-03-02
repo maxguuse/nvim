f8d7d0d1c57685aa50ce817f3303e15b0236e56f
maxguuse <max.pink.goose@gmail.com>
2025-03-02 19:08:44 +0300

    feat: replaced telescope with snacks.picker

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,3 +1,19 @@
+---@diagnostic disable-next-line: unused-local, unused-function
+local function get_cwd()
+	local git_root = vim.fn.systemlist("git rev-parse --show-toplevel")[1]
+
+	if git_root and git_root ~= "" and vim.v.shell_error == 0 then
+		return git_root
+	end
+
+	local workspace = vim.lsp.buf.list_workspace_folders()[1]
+	if workspace and workspace ~= "" then
+		return workspace
+	end
+
+	return vim.loop.cwd()
+end
+
 return {
 	"folke/snacks.nvim",
 	lazy = false,
@@ -13,6 +29,45 @@ return {
 			function()
 				require("snacks").toggle.zen():toggle()
 			end,
+		},
+		{
+			"<leader>pf",
+			function()
+				require("snacks").picker.files({
+					cwd = get_cwd(),
+				})
+			end,
+			desc = "Find Files",
+		},
+		{
+			"<leader>pg",
+			function()
+				require("snacks").picker.grep({
+					cwd = get_cwd(),
+				})
+			end,
+			desc = "Grep",
+		},
+		{
+			"<leader>pp",
+			function()
+				require("snacks").picker.projects()
+			end,
+			desc = "Projects",
+		},
+		{
+			"<leader>pk",
+			function()
+				require("snacks").picker.keymaps()
+			end,
+			desc = "Keymaps",
+		},
+		{
+			"<leader>pc",
+			function()
+				require("snacks").picker.lazy()
+			end,
+			desc = "Search for Plugin Spec",
 		},
 	},
 	opts = {
@@ -38,6 +93,7 @@ return {
 					and vim.bo[buf].buftype ~= "terminal"
 			end,
 		},
+		picker = {},
 		toggle = {},
 		dim = {},
 		zen = {},
