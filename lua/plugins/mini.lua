d4bf9dca125d1cac6e5e0db205e28512948cead2
maxguuse <max.pink.goose@gmail.com>
2025-03-02 20:28:52 +0300

    refactor: used mini.nvim instead of standalone mini plugins

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -31,6 +31,7 @@ local function statusline_arrow()
 		return string.format("󱡁%s", is_saved)
 	end
 end
+
 local function active_statusline()
 	local MiniStatusline = require("mini.statusline")
 
@@ -54,14 +55,9 @@ end
 
 return {
 	{
-		"echasnovski/mini.icons",
+		"echasnovski/mini.nvim",
 		version = false,
-		lazy = true,
-		opts = {},
-	},
-	{
-		"echasnovski/mini.files",
-		version = false,
+		lazy = false,
 		keys = {
 			{
 				"<leader>mf",
@@ -69,63 +65,38 @@ return {
 				"Mini Files",
 			},
 		},
-		opts = {},
-	},
-	{
-		"echasnovski/mini.pairs",
-		version = false,
-		event = "InsertEnter",
-		opts = {},
-	},
-	{
-		"echasnovski/mini.surround",
-		version = false,
-		event = "InsertEnter",
-		opts = {},
-	},
-	{
-		"echasnovski/mini-git",
-		version = false,
 		config = function()
+			require("mini.icons").setup()
+
+			require("mini.splitjoin").setup()
+			require("mini.cursorword").setup()
+
+			require("mini.pairs").setup()
+			require("mini.surround").setup()
+
+			require("mini.files").setup()
+
 			require("mini.git").setup()
-		end,
-	},
-	{
-		"echasnovski/mini.cursorword",
-		version = false,
-		opts = {},
-	},
-	{
-		"echasnovski/mini.hipatterns",
-		version = false,
-		config = function()
-			local hipatterns = require("mini.hipatterns")
-			hipatterns.setup({
+			require("mini.statusline").setup({
+				content = {
+					active = active_statusline,
+					inactive = inactive_statusline,
+				},
+
+				use_icons = true,
+				set_vim_settings = true,
+			})
+
+			require("mini.hipatterns").setup({
 				highlighters = {
 					fixme = { pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme" },
 					hack = { pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack" },
 					todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
 					note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
 
-					hex_color = hipatterns.gen_highlighter.hex_color(),
+					hex_color = require("mini.hipatterns").gen_highlighter.hex_color(),
 				},
 			})
 		end,
-	},
-	{
-		"echasnovski/mini.statusline",
-		version = false,
-		dependencies = {
-			"echasnovski/mini.icons",
-		},
-		opts = {
-			content = {
-				active = active_statusline,
-				inactive = inactive_statusline,
-			},
-
-			use_icons = true,
-			set_vim_settings = true,
-		},
 	},
 }
