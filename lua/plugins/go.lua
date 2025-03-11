66b585a31f125bc4de15a9fcd27ab4ec8a7274e5
maxguuse <max.pink.goose@gmail.com>
2025-03-11 17:38:38 +0300

    feat: removed refactoring.nvim

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -10,40 +10,4 @@ return {
 		ft = { "go", "gomod", "gowork" },
 		build = ':lua require("go.install").update_all_sync()',
 	},
-	{
-		"ThePrimeagen/refactoring.nvim",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-treesitter/nvim-treesitter",
-		},
-		opts = {
-			prompt_func_return_type = {
-				go = true,
-				java = false,
-
-				cpp = false,
-				c = false,
-				h = false,
-				hpp = false,
-				cxx = false,
-			},
-			prompt_func_param_type = {
-				go = true,
-				java = false,
-
-				cpp = false,
-				c = false,
-				h = false,
-				hpp = false,
-				cxx = false,
-			},
-			printf_statements = {},
-			print_var_statements = {},
-			show_success_message = false,
-		},
-		ft = { "go", "gomod", "gowork" },
-		keys = {
-			{ "<leader>rr", '<cmd>lua =require("telescope").extensions.refactoring.refactors()<CR>', "Undo tree" },
-		},
-	},
 }
