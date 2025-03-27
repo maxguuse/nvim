84bf425f461ac22d9c47cdc90d0e3da61e920781
maxguuse <max.pink.goose@gmail.com>
2025-03-27 05:18:16 +0300

    refactor: removed nvim-treesitter-textobjects module

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,9 +3,6 @@ return {
 	build = ":TSUpdate",
 	dependencies = {
 		{
-			"nvim-treesitter/nvim-treesitter-textobjects",
-		},
-		{
 			"nvim-treesitter/nvim-treesitter-context",
 			enabled = true,
 			opts = { mode = "cursor", max_lines = 3 },
@@ -45,59 +42,6 @@ return {
 				node_incremental = "<M-l>",
 				scope_incremental = false,
 				node_decremental = "<M-h>",
-			},
-		},
-		textobjects = {
-			select = {
-				enable = true,
-				lookahead = true,
-				kahead = true,
-				selection_modes = {
-					["@parameter.outer"] = "v", -- charwise
-					["@function.outer"] = "V", -- linewise
-					["@class.outer"] = "<c-v>", -- blockwise
-				},
-				keymaps = {
-					["aa"] = { query = "@parameter.outer", desc = "🌲select around function" },
-					["ia"] = { query = "@parameter.inner", desc = "🌲select inside function" },
-					["af"] = { query = "@function.outer", desc = "🌲select around function" },
-					["if"] = { query = "@function.inner", desc = "🌲select inside function" },
-					["ac"] = { query = "@class.outer", desc = "🌲select around class" },
-					["ic"] = { query = "@class.inner", desc = "🌲select inside class" },
-					["al"] = { query = "@loop.outer", desc = "🌲select around loop" },
-					["il"] = { query = "@loop.inner", desc = "🌲select inside loop" },
-					["ab"] = { query = "@block.outer", desc = "🌲select around block" },
-					["ib"] = { query = "@block.inner", desc = "🌲select inside block" },
-				},
-			},
-			move = {
-				enable = true,
-				set_jumps = true, -- whether to set jumps in the jumplist
-				goto_next_start = {
-					["]m"] = "@function.outer",
-					["]]"] = "@class.outer",
-				},
-				goto_next_end = {
-					["]M"] = "@function.outer",
-					["]["] = "@class.outer",
-				},
-				goto_previous_start = {
-					["[m"] = "@function.outer",
-					["[["] = "@class.outer",
-				},
-				goto_previous_end = {
-					["[M"] = "@function.outer",
-					["[]"] = "@class.outer",
-				},
-			},
-			lsp_interop = {
-				enable = true,
-				border = "rounded",
-				floating_preview_opts = {},
-				peek_definition_code = {
-					["<leader>df"] = "@function.outer",
-					["<leader>dF"] = "@class.outer",
-				},
 			},
 		},
 	},
