4bc3c62a1b4495c15969152dfa3149748d79b3be
maxguuse <max.pink.goose@gmail.com>
2025-03-31 00:48:06 +0300

    feat: some more work on folding styles

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -47,14 +47,3 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		vim.keymap.set("n", "<C-CR>", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
 	end,
 })
-
-vim.api.nvim_create_autocmd("LspAttach", {
-	group = ggoose_lsp,
-	callback = function(args)
-		local client = vim.lsp.get_client_by_id(args.data.client_id)
-		if client ~= nil and client:supports_method("textDocument/foldingRange") then
-			local win = vim.api.nvim_get_current_win()
-			vim.wo[win][0].foldexpr = "v:lua.vim.lsp.foldexpr()"
-		end
-	end,
-})
