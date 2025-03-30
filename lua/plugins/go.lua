8d0269b5d8debf9e689ee8596bb9689ae38621b5
maxguuse <max.pink.goose@gmail.com>
2025-03-30 09:12:01 +0300

    refactor: removed unused guihua.lua plugin

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -1,9 +1,6 @@
 return {
 	{
 		"ray-x/go.nvim",
-		dependencies = { -- optional packages
-			"ray-x/guihua.lua",
-		},
 		opts = {},
 		ft = { "go", "gomod", "gowork" },
 		build = ':lua require("go.install").update_all_sync()',
