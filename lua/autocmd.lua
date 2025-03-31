a0b9e96dd4d2eb9706f7cfb55d4b45a692e9a598
maxguuse <max.pink.goose@gmail.com>
2025-04-01 00:15:13 +0300

    refactor: minor changes to last dir functionality

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -2,11 +2,10 @@ local ggoose = vim.api.nvim_create_augroup("ggoose", { clear = true })
 vim.api.nvim_create_autocmd("BufEnter", {
 	group = ggoose,
 	callback = function()
-		local filename = os.getenv("NVIM_LAST_FILENAME")
-		if filename == nil then
+		local last_dir_file = os.getenv("NVIM_LAST_FILENAME")
+		if last_dir_file == nil then
 			return
 		end
-		local last_dir_file = os.getenv("HOME") .. "/.cache/nvim/last_dir/" .. filename
 		local current_dir = vim.fn.getcwd()
 		vim.fn.writefile({ current_dir }, last_dir_file)
 	end,
