9a4e264925c77491288f639bc1fbedf2726eb522
maxguuse <max.pink.goose@gmail.com>
2025-05-13 18:53:10 +0300

    refactor: starting from scratch

--- lua/command.lua
+++
@@ -1,1 +0,0 @@
-vim.api.nvim_create_user_command("MakeSession", require("core.sessions").make_new_session, {})
--- lua/plugins/go.lua
+++
@@ -1,8 +0,0 @@
-return {
-	{
-		"ray-x/go.nvim",
-		opts = {},
-		ft = { "go", "gomod", "gowork" },
-		build = ':lua require("go.install").update_all_sync()',
-	},
-}
