64c13120372ba3c0f6eaf207814e7a6d94d7470b
maxguuse <max.pink.goose@gmail.com>
2025-05-14 21:22:05 +0300

    feat: cloak

---
+++ lua/plugins/cloak.lua
@@ -0,0 +1,21 @@
+return {
+	"laytan/cloak.nvim",
+	lazy = true,
+	event = { "BufReadPre" },
+	config = function()
+		require("cloak").setup({
+			enabled = true,
+			cloak_character = "*",
+			highlight_group = "Comment",
+			cloak_length = 5,
+			try_all_patterns = true,
+			cloak_on_leave = false,
+			patterns = {
+				{
+					file_pattern = { "dev.env", ".env*" },
+					cloak_pattern = "=.+",
+				},
+			},
+		})
+	end,
+}
