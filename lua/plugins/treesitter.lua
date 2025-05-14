6bf34685d7968a4bdcedfc291226eb5920a3722b
maxguuse <max.pink.goose@gmail.com>
2025-05-14 16:04:02 +0300

    feat: treesitter

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,62 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  dependencies = {
+    {
+      "nvim-treesitter/nvim-treesitter-context",
+      enabled = true,
+      opts = { mode = "cursor", max_lines = 3 },
+    },
+  },
+  event = { "BufReadPost", "BufNewFile" },
+  cmd = { "TSInstall", "TSUninstall", "TSInstallSync" },
+  opts = {
+    ensure_installed = {
+      "go",
+      "gomod",
+      "gosum",
+      "gowork",
+      "gotmpl",
+      "lua",
+      "json",
+      "yaml",
+      "toml",
+      "dockerfile",
+      "sql",
+      "markdown",
+      "markdown_inline",
+      "vim",
+      "vimdoc",
+      "bash",
+      "hyprlang",
+      "regex",
+      "comment",
+    },
+
+    auto_install = true,
+    highlight = { enable = true },
+    indent = { enable = true },
+
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<M-l>",
+        node_incremental = "<M-l>",
+        scope_incremental = false,
+        node_decremental = "<M-h>",
+      },
+    },
+  },
+  config = function(_, opts)
+    vim.filetype.add({
+      extension = { rasi = "rasi" },
+      pattern = {
+        [".*/waybar/config"] = "jsonc",
+        [".*/kitty/*.conf"] = "bash",
+        [".*/hypr/.*%.conf"] = "hyprlang",
+      },
+    })
+
+    require("nvim-treesitter.configs").setup(opts)
+  end,
+}
