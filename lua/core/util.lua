16cbf501c733b6a2e956b3f2c434b601bedfeec2
maxguuse <max.pink.goose@gmail.com>
2025-05-15 15:32:48 +0300

    fix: simple adjustement of how GetProjectRoot() works

--- lua/core/util.lua
+++ lua/core/util.lua
@@ -1,16 +1,12 @@
 local M = {}
 
 function M.GetProjectRoot()
-  local git_root = vim.fs.root(0, '.git')
+  local git_root = vim.fs.root(0, { ".git", "Session.vim" })
 
-  if git_root and git_root ~= "" then
-    return git_root
-  end
+  if git_root and git_root ~= "" then return git_root end
 
   local workspace = vim.lsp.buf.list_workspace_folders()[1]
-  if workspace and workspace ~= "" then
-    return workspace
-  end
+  if workspace and workspace ~= "" then return workspace end
 
   return vim.loop.cwd()
 end
