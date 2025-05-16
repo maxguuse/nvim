33a86923ce2ea9e2e103c8c708f61a8ebe6a86b9
maxguuse <max.pink.goose@gmail.com>
2025-05-16 11:48:48 +0300

    feat: shame, but I decided to add nvim-ufo back in for the time being until mini.folds is born

--- lua/config/mini/hipatterns.lua
+++ lua/config/mini/hipatterns.lua
@@ -2,6 +2,7 @@ return {
   highlighters = {
     fixme = { pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme" },
     todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
+    note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
 
     hex_color = require("mini.hipatterns").gen_highlighter.hex_color(),
   },
--- lua/config/option.lua
+++ lua/config/option.lua
@@ -49,11 +49,13 @@ vim.opt.fillchars = { eob = " ", fold = " " }
 --- FOLDS -----------------------------------------------------------------------------
 vim.o.foldenable = true
 vim.o.foldcolumn = "0"
-vim.o.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 vim.o.foldlevel = 99
 vim.o.foldlevelstart = 99
-vim.o.foldmethod = "expr"
-vim.o.foldtext = ""
+
+--- NOTE: Commented out for ufo.nvim purposes
+-- vim.o.foldmethod = "expr"
+-- vim.o.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+-- vim.o.foldtext = ""
 
 --- EDITING ---------------------------------------------------------------------------
 vim.o.autoindent = true
