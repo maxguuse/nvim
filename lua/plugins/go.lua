a3877ffde8e7cd43bac48a184cfa091543d97faa
maxguuse <max.pink.goose@gmail.com>
2025-05-16 09:21:51 +0300

    fix: changed tabs for spaces in some files

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -1,8 +1,8 @@
 return {
-	{
-		"ray-x/go.nvim",
-		opts = {},
-		ft = { "go", "gomod", "gowork" },
-		build = ':lua require("go.install").update_all_sync()',
-	},
+  {
+    "ray-x/go.nvim",
+    opts = {},
+    ft = { "go", "gomod", "gowork" },
+    build = ':lua require("go.install").update_all_sync()',
+  },
 }
